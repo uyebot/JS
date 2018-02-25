3824a9fb62eabc21298f5a447c3ee8fd45537dc9
wawnuye <32885552+uyebot@users.noreply.github.com>
2018-02-25 17:20:05 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,42 +1,55 @@
-const LineConnect = require('./connect');
-const LINE = require('./main.js');
-console.info("\n\
-=========================================\n\
-BotName: LINE Alphat JS\n\
-Version: FORKED VERSION\n\
-Thanks to @Alfathdirk @TCR_TEAM\n\
-=========================================\n\
-\nNOTE : This bot is made by @Alfathdirk @TCR_TEAM and has been forked by @GoogleX !\n\
-***Copyright belongs to the author***");
+const { hostname, platform } = require('os');
 
-/*
-| This constant is for auth/login
-| 
-| Change it to your authToken / your email & password
-*/
-const auth = {
-	authToken: '',
-	certificate: '',
-	email: '',
-	password: ''
-}
+const whichPlatform = platform() === 'darwin' ? 'MAC' : 'win32';
 
-let client =  new LineConnect();
-//let client =  new LineConnect(auth);
+const config = {
+  LINE_DOMAIN_INDO: 'ga2i.line.naver.jp',
+  LINE_DOMAIN_FAST: 'gfv.line.naver.jp',
+  LINE_DOMAIN_TOOFAST: 't.line.naver.jp',
+  LINE_DOMAIN: 'gf.line.naver.jp',
+  LINE_DOMAIN_2ND: 'gd2.line.naver.jp',
+  LINE_DOMAIN_3RD: 'gfs.line.naver.jp', 
+  LINE_DOMAIN_4TH: 'gfps.line.naver.jp',
+  LINE_KEEP: '/kp/api/v21/keep', //Keep -> gfs.line.naver.jp/kp/api/v21/keep/sync.json?revision=0&limit=50&serviceType=1 || fetch.json?startRevision=0&limit=50&serviceType=1
+  LINE_OS_URL: 'os.line.naver.jp',
+  LINE_HTTP_URL: '/api/v4/TalkService.do',
+  LINE_HTTP_URL_2ND: '/api/v3/TalkService.do',
+  LINE_RS: '/api/v4p/rs',
+  LINE_STICKER_URL: 'dl.stickershop.line.naver.jp/products/',
+  LINE_API_QUERY_PATH_SEC: '/F4',
+  LINE_POLL_QUERY_PATH_SEC: '/E4',
+  LINE_POLL_QUERY_PATH_THI: '/H4',
+  LINE_NORMAL_POLL_QUERY_PATH: '/NP4',
+  LINE_COMPACT_MESSAGE_QUERY_PATH: '/C5',
+  LINE_CALL_QUERY_PATH: '/V4',
+  LINE_POLL_URL: '/P4',
+  LINE_POLL_URL_2ND: '/P3',
+  LINE_COMMAND_PATH: '/S4',
+  LINE_COMMAND_PATH_2: '/S3',
+  LINE_CERTIFICATE_URL: '/Q',
+  LINE_CPF: '/CPF', //What is this ?
+  LINE_CHANNEL_PATH: '/CH4',
+  LINE_SHOP_PATH: '/SHOP4',
+  LINE_SESSION_LINE_URL: '/authct/v1/keys/line',
+  LINE_SESSION_NAVER_URL: '/authct/v1/keys/naver',
+  LINE_POST_CONTENT_URL: 'https://os.line.naver.jp/talk/m/upload.nhn',
+  LINE_POST_CONTENT_URL_2ND: 'https://obs-sg.line-apps.com/talk/m/upload.nhn',
+  //X_LINE_APP: 'IOSIPAD\t7.18.0\tiPhone OS\t11.12.1',
+  X_LINE_APP: 'CHROMEOS\t1.4.17\tChrome_OS\t1',
+  ip: '127.0.0.1',
+  version: '0.0.2',
+  revision: 0,
+  hostname: hostname(),
+  platform: whichPlatform,
+  EMAIL_REGEX: /[^@]+@[^@]+\.[^@]+/,
+  Headers: {
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+  },
+  YT_DL: 'http://www.saveitoffline.com/process/',
+  tokenn: '',
+  chanToken: '',
+  botmid: '',
+  doing: 'no'
+};
 
-client.startx().then(async (res) => {
-	while(true) {
-		try {
-			ops = await client.fetchOps(res.operation.revision);
-		} catch(error) {
-			console.log('error',error)
-		}
-		for (let op in ops) {
-			if(ops[op].revision.toString() != -1){
-				res.operation.revision = ops[op].revision;
-				LINE.poll(ops[op])
-			}
-		}
-		//LINE.aLike() //AutoLike (CAUSE LAG)
-	}
-});
+module.exports = config;
