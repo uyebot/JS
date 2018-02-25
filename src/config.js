84cc84616b6cf6b1924761c0998b0ae3fbbd2ddf
wawnuye <32885552+uyebot@users.noreply.github.com>
2018-02-25 17:19:13 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,29 +1,42 @@
-const { hostname, platform } = require('os');
+const LineConnect = require('./connect');
+const LINE = require('./main.js');
+console.info("\n\
+=========================================\n\
+BotName: LINE Alphat JS\n\
+Version: FORKED VERSION\n\
+Thanks to @Alfathdirk @TCR_TEAM\n\
+=========================================\n\
+\nNOTE : This bot is made by @Alfathdirk @TCR_TEAM and has been forked by @GoogleX !\n\
+***Copyright belongs to the author***");
 
-const whichPlatform = platform() === 'darwin' ? 'MAC' : 'win32';
+/*
+| This constant is for auth/login
+| 
+| Change it to your authToken / your email & password
+*/
+const auth = {
+	authToken: '',
+	certificate: '',
+	email: '',
+	password: ''
+}
 
-const config = {
-  LINE_DOMAIN: 'gf.line.naver.jp',
-  // LINE_DOMAIN: 'gd2.line.naver.jp',
-  LINE_OS_URL: 'os.line.naver.jp',
-  LINE_HTTP_URL: '/api/v4/TalkService.do',
-  LINE_STICKER_URL: 'dl.stickershop.line.naver.jp/products/',
-  LINE_POLL_URL: '/P4',
-  LINE_COMMAND_PATH: '/S4',
-  LINE_CERTIFICATE_URL: '/Q',
-  LINE_SHOP_PATH: '/SHOP4',
-  LINE_SESSION_LINE_URL: '/authct/v1/keys/line',
-  LINE_SESSION_NAVER_URL: '/authct/v1/keys/naver',
-  LINE_POST_CONTENT_URL: 'http://os.line.naver.jp/talk/m/upload.nhn',
-  ip: '127.0.0.1',
-  version: '0.0.2',
-  revision: 0,
-  hostname: hostname(),
-  platform: whichPlatform,
-  EMAIL_REGEX: /[^@]+@[^@]+\.[^@]+/,
-  Headers: {
-    'User-Agent':'DESKTOP:MAC:10.10.2-YOSEMITE-x64(4.5.0)'
-  }
-};
+let client =  new LineConnect();
+//let client =  new LineConnect(auth);
 
-module.exports = config;
+client.startx().then(async (res) => {
+	while(true) {
+		try {
+			ops = await client.fetchOps(res.operation.revision);
+		} catch(error) {
+			console.log('error',error)
+		}
+		for (let op in ops) {
+			if(ops[op].revision.toString() != -1){
+				res.operation.revision = ops[op].revision;
+				LINE.poll(ops[op])
+			}
+		}
+		//LINE.aLike() //AutoLike (CAUSE LAG)
+	}
+});
